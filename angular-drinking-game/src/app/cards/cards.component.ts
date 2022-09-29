e38281d0eb224d96698424eafd276292b4506ce0
Julius <AA2850@student.jamk.fi>
2022-09-29 13:16:00 +0300

    The cards works!!
    thanks to mr Pölkki

--- angular-drinking-game/src/app/cards/cards.component.ts
+++ angular-drinking-game/src/app/cards/cards.component.ts
@@ -1,14 +1,35 @@
-import { Component, OnInit} from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef} from '@angular/core';
 
   @Component({
     selector: 'app-cards',
     templateUrl: './cards.component.html',
     styleUrls: ['./cards.component.css']
   })
+  
 export class CardsComponent implements OnInit {
   constructor () {};
-  
+  @ViewChild('canvas', { static: true }) 
+  // @ts-ignore
+  canvas: ElementRef<HTMLCanvasElement>|ElementRef;
+  // @ts-ignore
+  c: CanvasRenderingContext2D|CanvasRenderingContext2D;
+
   ngOnInit(): void {
+    this.c = this.canvas.nativeElement.getContext('2d');
+    this.c.font = "60px Roboto-Black, sans-serif";
+    this.c.fillText("♠", 20, 110);
+
+    this.c.font = "50px Roboto-Black, sans-serif";
+    this.c.fillText("2", 20, 60);
+
+    this.c.font = "60px Roboto-Black, sans-serif";
+    this.c.rotate(180 * Math.PI / 180);
+    this.c.fillText("♠", -280, -360);
+
+
+    this.c.font = "50px Roboto-Black, sans-serif";
+    this.c.fillText("2", -280, -410);
+
   }
 
 }
