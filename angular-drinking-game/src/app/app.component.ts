e17bd974772653e241eb63de7a3ad9f0149f08bc
AA3098 <79079098+AA3098@users.noreply.github.com>
2022-10-06 10:09:11 +0300

    made working modal component
    Made a working modal component that can be used to make any component a modal component if needed

--- angular-drinking-game/src/app/app.component.ts
+++ angular-drinking-game/src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
+import { ModalService } from './_modal';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'angular-drinking-game';
+
+  constructor(public modalService: ModalService) {}
 }
--- angular-drinking-game/src/app/app.module.ts
+++ angular-drinking-game/src/app/app.module.ts
@@ -6,14 +6,11 @@ import { AppComponent } from './app.component';
 import { DeckComponent } from './game-table/game-one/deck/deck.component';
 import { CardsComponent } from './game-table/game-one/cards/cards.component';
 
+import { ModalModule } from './_modal';
+
 @NgModule({
-  declarations: [
-    AppComponent,
-    DeckComponent,
-    CardsComponent,
-    routingComponents,
-  ],
-  imports: [BrowserModule, AppRoutingModule],
+  declarations: [AppComponent, DeckComponent, CardsComponent, routingComponents],
+  imports: [BrowserModule, AppRoutingModule, ModalModule],
   providers: [],
   bootstrap: [AppComponent],
 })
