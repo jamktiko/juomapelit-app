2924dc0a2b36d4a88189fc6a6cd2a019a8f088c5
Konnad <AA4110@student.jamk.fi>
2022-10-10 11:08:11 +0300

    Added create lobby component
    Added create lobby component

--- angular-drinking-game/src/app/app.module.ts
+++ angular-drinking-game/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppComponent } from './app.component';
 
 import { DeckComponent } from './game-table/game-one/deck/deck.component';
 import { CardsComponent } from './game-table/game-one/cards/cards.component';
+import { CreateLobbyComponent } from './join-table/create-lobby/create-lobby.component';
 
 @NgModule({
   declarations: [
@@ -12,6 +13,7 @@ import { CardsComponent } from './game-table/game-one/cards/cards.component';
     DeckComponent,
     CardsComponent,
     routingComponents,
+    CreateLobbyComponent,
   ],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
