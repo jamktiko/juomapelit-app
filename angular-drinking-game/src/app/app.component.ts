d53f8b298ea980a869571cad984d05d424fb3a27
Julius <AA2850@student.jamk.fi>
2022-10-28 13:11:07 +0300

    Layout updates
    Animated background, new logo and tiny color changes.

--- angular-drinking-game/src/app/app.component.ts
+++ angular-drinking-game/src/app/app.component.ts
@@ -1,10 +1,27 @@
 import { Component } from '@angular/core';
 import { ModalService } from './_modal';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+@NgModule({
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule
+  ],
+  declarations: [ ],
+  bootstrap: [ ]
+})
+export class AppModule { }
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
+  animations: [
+    // animation triggers go here
+
+  ]
 })
 export class AppComponent {
   title = 'angular-drinking-game';
