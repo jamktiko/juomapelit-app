700db6d7cd6c63e184e6bc4e48b3e1fbd4616ddf
Aku Järvinen <aa3953@student.jamk.fi>
2022-11-01 09:29:10 +0200

    Jii jii

--- angular-drinking-game/cypress/e2e/spec.cy.ts
+++ angular-drinking-game/cypress/e2e/spec.cy.ts
@@ -4,31 +4,48 @@ describe('Testataan peliin liitymistä', () => {
   it('passes', () => {
     cy.visit('http://localhost:4200/MainMenu')
     cy.wait(1000)
-    cy.get('button.button1').click({multiple: true})
+    cy.get('button#play.button1').click()
     cy.wait(1000)
-    cy.get('button.button1').click({multiple: true, force: true})
+    cy.get('button#joinGame.button1').click()
     cy.wait(1000)
-    cy.get('button.button1').click({multiple: true, force: true})
+    cy.get('button#lobbyJoin').click()
     cy.wait(1000)
-    cy.get('button.button2').click({multiple:true, force: true})
+    cy.get('button#chooseGame.button2').click()
     cy.wait(1000)
-    cy.get('button.button2').click({multiple:true, force: true})
-    
+    cy.get('button#vesiputous.button2').click()
+    cy.wait(1000)
   })
 
 })
+
 
 describe('Testataan pelin luomista', () => {
   it('passes', () => {
     cy.visit('http://localhost:4200/MainMenu')
+    cy.wait(1000)
+    cy.get('button#play.button1').click()
+    cy.wait(1000)
+    cy.get('button#createGame.button1').click()
+    cy.wait(1000)
+    cy.get('button#vesiputous.button2').click()
+    cy.wait(1000)
+
   })
 
 })
+
 
 describe('Testataan että kortteja voidaan klikata', () => {
   it('passes', () => {
     cy.visit('http://localhost:4200/GameOne')
+<<<<<<< Updated upstream
     cy.wait(3000)
     cy.get('canvas#cardBack').click({multiple: true, force: true})
+=======
+    for(let i = 0; i <= 52; i++){
+    cy.get('canvas#cardBack').click({force: true})
+    }
+>>>>>>> Stashed changes
   })
 })
+
