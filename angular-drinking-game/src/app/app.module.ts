8e880cf23ce0ba89f38ad4d9f05123cbee4c91c3
Konnad <AA4110@student.jamk.fi>
2022-11-01 09:27:51 +0200

    minor changes
    deleted testing and added routing things back in because visual studio started to ryttyillä

--- angular-drinking-game/src/app/app.module.ts
+++ angular-drinking-game/src/app/app.module.ts
@@ -2,11 +2,10 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule, routingComponents } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { RouterModule } from '@angular/router';
 import { DeckComponent } from './game-table/game-one/deck/deck.component';
 import { CardsComponent } from './game-table/game-one/cards/cards.component';
 import { CreateLobbyComponent } from './join-table/create-lobby/create-lobby.component';
-import { TestingComponent } from './testing/testing.component';
 
 
 import { ModalModule } from './_modal';
@@ -18,7 +17,6 @@ import { ModalModule } from './_modal';
     CardsComponent,
     routingComponents,
     CreateLobbyComponent,
-    TestingComponent,
   ],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
