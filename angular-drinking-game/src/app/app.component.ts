35c178575a12047dc3ccdd8021033bd5499fdfb5
Julius <AA2850@student.jamk.fi>
2022-11-10 13:08:13 +0200

    janne chan make checkbox validation work

--- angular-drinking-game/src/app/app.component.ts
+++ angular-drinking-game/src/app/app.component.ts
@@ -2,7 +2,6 @@ import { Component } from '@angular/core';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { delay } from 'rxjs';
 
 @NgModule({
   imports: [
@@ -25,12 +24,21 @@ export class AppModule { }
 })
 export class AppComponent {
   title = 'BrewDeck';
+  valid = false;
+  over18 = document.getElementById("over18") as HTMLInputElement | null;
+  respDrink = document.getElementById("respDrink") as HTMLInputElement | null;
+  drinkWater = document.getElementById("drinkWater") as HTMLInputElement | null;
   ngOnInit() {
     this.openModal();
   }
   openModal() {
     document.getElementById('modalButton')?.click();
     console.log('modal opened');
+  }
+  validate() {
+    if (this.over18?.checked == true && this.respDrink?.checked == true && this.drinkWater?.checked == true) {
+      this.valid === true;
+    }
   }
   constructor() {}
 
