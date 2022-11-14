d1cb606fcf9f227ffc3ff1e9efe60b9acf0ab13e
Konnad <AA4110@student.jamk.fi>
2022-11-14 13:50:08 +0200

    emailjs integration

--- angular-drinking-game/src/app/contact/contact.component.ts
+++ angular-drinking-game/src/app/contact/contact.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import emailjs, { EmailJSResponseStatus } from '@emailjs/browser';
 
 @Component({
   selector: 'app-contact',
@@ -8,7 +9,18 @@ import { Component, OnInit } from '@angular/core';
 export class ContactComponent implements OnInit {
 
   constructor() { }
-
+  submitted = false;
+  public sendEmail(e: Event) {
+    e.preventDefault();
+    emailjs.sendForm('service_nzqqia9', 'template_pkkwgxn', e.target as HTMLFormElement, 'VvNpmdrxMAxW2nvcX')
+      .then((result: EmailJSResponseStatus) => {
+        console.log(result.text);
+        console.log("tämä onnistui");
+      }, (error) => {
+        console.log(error.text);
+        console.log("tämä epäonnistui");
+      });
+    }
   collapsed: boolean = true;
 
   openForm() {
@@ -17,6 +29,10 @@ export class ContactComponent implements OnInit {
 
   closeForm() {
     this.collapsed = true;
+  }
+
+  submitAndReturn() {
+    this.submitted = true;
   }
   ngOnInit(): void {
   }
