724f2e2c2c6bbbb6c56bc3f79c6c2aac3a516f7b
Aku Järvinen <aa3953@student.jamk.fi>
2022-11-23 14:26:33 +0200

    Minor changes... lisäsin id:t kuviin ja painikkeeseen :D

--- angular-drinking-game/cypress/e2e/akunTestit/akuntesti.cy.js
+++ angular-drinking-game/cypress/e2e/akunTestit/akuntesti.cy.js
@@ -4,39 +4,25 @@ const { describe } = require("mocha");
 
 describe("Testataan että käyttäjä pääsee peliin", () => {
   beforeEach(() => {
-    cy.visit("http://localhost:4200");
+    cy.visit("http://brewdeck.click");
   });
 
   it("Testataan että käyttäjä pääsee peliin", () => {
-    cy.get("button#play.button1").click();
-    cy.wait(2000);
-    cy.get("button#joinGameButton.button1").click();
-    cy.wait(2000);
+    cy.wait(1500);
+    cy.get("button.button1").scrollIntoView();
+    cy.wait(1500);
     cy.get("button.button1").click();
-    cy.wait(2000);
-    cy.get("button#chooseGame.button2").click();
-    cy.wait(2000);
-    cy.get("button#vesiputous.button2").click();
-    cy.wait(2000);
-  });
-});
-
-describe("Testataan että pelinäkymässä kortteja voidaan klikata", () => {
-  beforeEach(() => {
-    cy.visit("http://localhost:4200/GameOne");
-  });
-
-  it("Testataan että pelinäkymässä kortteja voidaan klikata", () => {
-    cy.get("canvas#canvas").click({ force: true });
-  });
-});
-
-describe("Testataan että pelinäkymässä pääsee takaisin etusivulle", () => {
-  beforeEach(() => {
-    cy.visit("http://localhost:4200/GameOne");
-  });
-  it("Testataan että pelinäkymässä pääsee takaisin etusivulle", () => {
     cy.wait(5000);
     cy.get("canvas#canvas").click({ force: true });
+    cy.wait(1000);
+    cy.get("canvas#canvas").click({ force: true });
+    cy.wait(1000);
+    cy.get("canvas#canvas").click({ force: true });
   });
+
+  beforeEach(() => {
+    cy.visit("http://brewdeck.click");
+  });
+
+  it("Testataan että somelinkit toimii", () => {});
 });
