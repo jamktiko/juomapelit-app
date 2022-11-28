79fb19b62845a3ebbeb13967de8514390c400042
Aku Järvinen <aa3953@student.jamk.fi>
2022-11-28 13:21:47 +0200

    asdasd

---
+++ angular-drinking-game/cypress/e2e/test0.5.2.cy.js
@@ -0,0 +1,36 @@
+//Testataan että käyttäjä pääsee peliin
+
+const { describe } = require("mocha");
+
+describe("Pelitesti", () => {
+  beforeEach(() => {
+    cy.visit("http://brewdeck.click");
+  });
+
+  it("Testataan että päästään peliin, peli tomii ja että pelaaja voi aloittaa uuden pelin.", () => {
+    cy.wait(1500);
+    cy.get("button.button1").scrollIntoView();
+    cy.wait(1500);
+    cy.get("button.button1").click();
+    cy.wait(5000);
+    for (let i = 0; i <= 102; i++) {
+      cy.get("canvas#canvas").click({ force: true });
+      cy.wait(25);
+    }
+    cy.get("button.button1").scrollIntoView();
+    cy.wait(1500);
+    cy.get("button.button1").click();
+    cy.url().should("include", "https://brewdeck.click/GameOne");
+    cy.wait(1000);
+  });
+
+  it("Testataan että päästään takaisin etusivulle", () => {
+    cy.wait(1500);
+    cy.visit("https://brewdeck.click/GameOne");
+    cy.wait(5000);
+    cy.get("div#exit.iconBtn.iconRight.iconRed").click();
+    cy.wait(1500);
+    cy.url().should("include", "https://brewdeck.click/");
+    cy.wait(1000);
+  });
+});
