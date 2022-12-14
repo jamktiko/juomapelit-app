e1970c824065c1deba1772c10dbe0263b5efd816
Julius <AA2850@student.jamk.fi>
2022-12-14 11:05:06 +0200

    new updates

--- angular-drinking-game/src/app/game-table/game-one/cards/cards.component.ts
+++ angular-drinking-game/src/app/game-table/game-one/cards/cards.component.ts
@@ -98,17 +98,40 @@ export class CardsComponent implements OnInit {
     this.addSuitRank();
   }
 
+  spade:string = '&#9830;&#xFE0E;';
+  club:string = '&#9827;&#xFE0E;';
+  heart:string = '&#10084;&#xFE0E;';
+  diamond:string = '&#9830;&#xFE0E;';
+  
   // Draw suit and rank to card
   addSuitRank() {
     this.c.textAlign = "center";
     // Adds suit and rank to top
-    this.c.font = "60px Roboto-Black, sans-serif";
+    this.c.font = "50px Roboto-Black, sans-serif";
         // Hearts and diamonds color red, spades and clubs black
         if (this.shuffledCards[0][0]['suit'] === "♦" || this.shuffledCards[0][0]['suit'] === "♥") {
           this.c.fillStyle = "red";
         } else {
           this.c.fillStyle = "black";
         };
+        // Changing the symbols to UTF-8 to prevent emojis in Android devices
+        // diamond
+        if (this.shuffledCards[0][0]['suit'] === "♦") {
+          this.shuffledCards[0][0]['suit'] === this.diamond;
+        }
+        // heart
+        if (this.shuffledCards[0][0]['suit'] === "♥") {
+          this.shuffledCards[0][0]['suit'] === this.heart;
+        }
+        // spade
+        if (this.shuffledCards[0][0]['suit'] === "♠") {
+          this.shuffledCards[0][0]['suit'] === this.spade;
+        }
+        // club
+        if (this.shuffledCards[0][0]['suit'] === "♣") {
+          this.shuffledCards[0][0]['suit'] === this.club;
+        } 
+  
         // Changes ranks 11/12/13/1 to J/Q/K/A
         if (this.shuffledCards[0][0]['rank'] == 11) {
           this.shuffledCards[0][0]['rank'] = "J";
@@ -119,34 +142,17 @@ export class CardsComponent implements OnInit {
         } else if (this.shuffledCards[0][0]['rank'] == 1) {
           this.shuffledCards[0][0]['rank'] = "A";
         };
-        
-        // Changes the suit (string) to unicode. So it can be displayed right on mobile
-        if (this.shuffledCards[0][0]['suit'] == "♦") {
-          this.c.font = "60px Roboto-Black, sans-serif";
-          this.c.fillText('\u{2666}\u{FE0E}', 35, 110);
-          this.c.fillText('\u{2666}\u{FE0E}', -265, -360);
-        } else if (this.shuffledCards[0][0]['suit'] == "♥") {
-          this.c.font = "60px Roboto-Black, sans-serif";
-          this.c.fillText('\u{2665}\u{FE0E}', 35, 110);
-          this.c.fillText('\u{2665}\u{FE0E}', -265, -360);
-        } else if (this.shuffledCards[0][0]['suit'] == "♠") {
-          this.c.font = "60px Roboto-Black, sans-serif";
-          this.c.fillText('\u{2660}\u{FE0E}', 35, 110);
-          this.c.fillText('\u{2660}\u{FE0E}', -265, -360);
-        } else if (this.shuffledCards[0][0]['suit'] == "♣") {
-          this.c.font = "60px Roboto-Black, sans-serif";
-          this.c.fillText('\u{2663}\u{FE0E}', 35, 110);
-          this.c.fillText('\u{2663}\u{FE0E}', -265, -360);
-        };
-    
+    this.c.fillText(this.shuffledCards[0][0]['suit'], 35, 110);
     this.c.font = "50px Roboto-Black, sans-serif";
     this.c.fillText(this.shuffledCards[0][0]['rank'], 35, 60);
     // Adds suit and rank to bottom upside down
     this.c.rotate(180 * Math.PI / 180);
-
+    this.c.font = "50px Roboto-Black, sans-serif";
+    this.c.fillText(this.shuffledCards[0][0]['suit'], -265, -360);
     this.c.font = "50px Roboto-Black, sans-serif";
     this.c.fillText(this.shuffledCards[0][0]['rank'], -265, -410);
   }
+  
 
   // Plays the next card when the user clicks the button to do so
   numcount = 0;
--- angular-drinking-game/src/app/game-table/game-two/cards2/cards2.component.ts
+++ angular-drinking-game/src/app/game-table/game-two/cards2/cards2.component.ts
@@ -108,14 +108,14 @@ cardFrontside() {
 
 spade:string = '&#9830;&#xFE0E;';
 club:string = '&#9827;&#xFE0E;';
-heart:string = '&#9824;&#xFE0E;';
+heart:string = '&#10084;&#xFE0E;';
 diamond:string = '&#9830;&#xFE0E;';
 
 // Draw suit and rank to card
 addSuitRank() {
   this.c.textAlign = "center";
   // Adds suit and rank to top
-  this.c.font = "60px Roboto-Black, sans-serif";
+  this.c.font = "50px Roboto-Black, sans-serif";
       // Hearts and diamonds color red, spades and clubs black
       if (this.shuffledCards[0][0]['suit'] === "♦" || this.shuffledCards[0][0]['suit'] === "♥") {
         this.c.fillStyle = "red";
@@ -138,7 +138,7 @@ addSuitRank() {
       // club
       if (this.shuffledCards[0][0]['suit'] === "♣") {
         this.shuffledCards[0][0]['suit'] === this.club;
-      }
+      } 
 
       // Changes ranks 11/12/13/1 to J/Q/K/A
       if (this.shuffledCards[0][0]['rank'] == 11) {
@@ -155,7 +155,7 @@ addSuitRank() {
   this.c.fillText(this.shuffledCards[0][0]['rank'], 35, 60);
   // Adds suit and rank to bottom upside down
   this.c.rotate(180 * Math.PI / 180);
-  this.c.font = "60px Roboto-Black, sans-serif";
+  this.c.font = "50px Roboto-Black, sans-serif";
   this.c.fillText(this.shuffledCards[0][0]['suit'], -265, -360);
   this.c.font = "50px Roboto-Black, sans-serif";
   this.c.fillText(this.shuffledCards[0][0]['rank'], -265, -410);
